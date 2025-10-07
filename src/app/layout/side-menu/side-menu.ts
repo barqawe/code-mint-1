a1b7d16c032bbb01e3962896985ea3aea28a2231
wesam abdulfattah <wesam.barqawe@gmail.com>
2025-10-07 23:43:39 +0300

    0

--- src/app/layout/side-menu/side-menu.ts
+++ src/app/layout/side-menu/side-menu.ts
@@ -1,22 +1,81 @@
-import { Component, Output,EventEmitter, inject, signal, effect } from '@angular/core';
+import { Component, Output, EventEmitter, inject, signal, effect } from '@angular/core';
 import { NgClass } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { Layout } from '../../layout';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-side-menu',
-  imports: [NgClass, RouterLink, RouterLinkActive],
+  imports: [NgClass, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './side-menu.html',
   styleUrl: './side-menu.scss'
 })
 export class SideMenu {
-  constructor(public layout:Layout) {
+  constructor(public layout: Layout) {
   }
 
   toggleMenu() {
     this.layout.toggleSideMenu();
   }
-  
-  }
+
+  menuItems = [
+    {
+      routerLink: '/',
+      isActiveIcon: '/side-menu-icons/active-academic-year.svg',
+      inactiveIcon: '/side-menu-icons/non-active-academic-year.svg',
+      text: 'Event Form',
+      exact: true,
+      isActive: false
+    },
+    {
+      routerLink: null,
+      isActiveIcon: '/side-menu-icons/non-active-graduate-attributes.svg',
+      inactiveIcon: '/side-menu-icons/non-active-graduate-attributes.svg',
+      text: 'Grad Attributes',
+      exact: false,
+      isActive: false
+    },
+    {
+      routerLink: '/nqfd',
+      isActiveIcon: '/side-menu-icons/active-national-qualifications-framework-descriptors.svg',
+      inactiveIcon: '/side-menu-icons/non-active-national-qualifications-framework-descriptors.svg',
+      text: 'NQF Descriptors',
+      exact: true,
+      isActive: false
+    },
+    {
+      routerLink: null,
+      isActiveIcon: '/side-menu-icons/non-active-evaluation-criterion.svg',
+      inactiveIcon: '/side-menu-icons/non-active-evaluation-criterion.svg',
+      text: 'Evaluation criterion',
+      exact: false,
+      isActive: false
+    },
+    {
+      routerLink: null,
+      isActiveIcon: '/side-menu-icons/non-active-program.svg',
+      inactiveIcon: '/side-menu-icons/non-active-program.svg',
+      text: 'Programs',
+      exact: false,
+      isActive: false
+    },
+    {
+      routerLink: null,
+      isActiveIcon: '/side-menu-icons/non-active-course.svg',
+      inactiveIcon: '/side-menu-icons/non-active-course.svg',
+      text: 'Courses',
+      exact: false,
+      isActive: false
+    },
+    {
+      routerLink: null,
+      isActiveIcon: '/side-menu-icons/non-active-graduate-attributes.svg',
+      inactiveIcon: '/side-menu-icons/non-active-graduate-attributes.svg',
+      text: 'C.Sections',
+      exact: false,
+      isActive: false
+    }
+  ];
+}
 
 
