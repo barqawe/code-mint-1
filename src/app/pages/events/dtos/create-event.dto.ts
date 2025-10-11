38cc2273cc0ffba84363dec6b804d5d757fe653b
wesam abdulfattah <wesam.barqawe@gmail.com>
2025-10-11 16:50:08 +0300

    worked on the dtos

--- src/app/pages/events/dtos/create-event.dto.ts
+++ src/app/pages/events/dtos/create-event.dto.ts
@@ -1,4 +1,22 @@
 export class CreateEventDto {
+  eventNameEn: string = '';
+  eventNameAr: string = '';
+  eventDescriptionEn: string = '';
+  eventDescriptionAr: string = '';
+  eventDate: string = '';
+  eventTime: string = '';
+  active: boolean = true;
+  eventPrice: number = 0;
+  eventSubCategory: string = '';
+  eventAvailableQuantity: number = 0;
+  eventSections?: Array<{
+    sectionTitle: string;
+    sectionItems: Array<{
+      itemNameAr: string;
+      itemNameEn: string;
+    }>;
+  }> = [];
+  eventImages: string[] = [];
 
-    
+
 }
